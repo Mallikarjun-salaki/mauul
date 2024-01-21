38e9a103c80d9c2a0a6263645742c8b0b597a155
Mallikarjun-salaki <mallikarjunsalaki0@gmail.com>
2024-01-21 23:18:55 +0530

    first message

---
+++ x.java
@@ -0,0 +1,14 @@
+class malik 
+{
+    public static void main(String[] args) 
+    {
+        
+        int i = 10;
+        Integer j = Integer.valueOf(i);
+        System.out.println(j);
+        
+        String s ="56786";
+        int k = Integer.parseInt(s);
+        System.out.println(s);
+        }
+}
